6c74d9e4cf3620ee5e2d6e7b9948a10a6b478be0
Paras Doshi <84129412+paras953@users.noreply.github.com>
2024-10-18 21:55:20 +0530

    use yfinance to download historical data (#2)

--- analytics/adhocrunfile.py
+++ analytics/adhocrunfile.py
@@ -1,30 +1,12 @@
-import os
-import re
 import pandas as pd
+import yfinance as yf
+import os
 
-def _extract_dividend_amount(description: str, face_value: float):
-    # Function to extract dividend amount
-
-    # Check for percentage pattern, e.g., "Dividend 20%"
-    percentage_match = re.search(r'(\d+)%', description)
-    if percentage_match:
-        percent_value = float(percentage_match.group(1))
-        return (percent_value / 100) * face_value
-
-    # Check for absolute amount pattern, e.g., "Dividend Rs 5" or "Re 1"
-    absolute_match = re.search(r'Rs.\s?(\d*\.?\d+)|Re\s?(\d*\.?\d+)', description)
-    if absolute_match:
-        # Extract either Rs or Re value
-        amount_value = float(absolute_match.group(1) or absolute_match.group(2))
-        return amount_value
-
-        # Return NaN if no dividend information found
-    return None
-
+from yfinance.utils import auto_adjust
 
 if __name__ == '__main__':
-    path = r'C:\Users\paras\PycharmProjects\EQQuant\additional_data\nifty50_corporate_actions\dividend_nifty50_stocks.csv'
-    div_df = pd.read_csv(path)
-    div_df['dividend_amount'] = div_df.apply(
-                lambda x: _extract_dividend_amount(x['PURPOSE'], x['FACE VALUE']), axis=1)
-    div_df.to_csv(r'C:\Users\paras\PycharmProjects\EQQuant\additional_data\nifty50_corporate_actions\dividend_nifty50_stocks_processed.csv')
+    symbol = 'SBIN'
+    yf_ticker = symbol + ".NS"
+    ticker = yf.Ticker(yf_ticker)
+    hist_prices = ticker.history(start='2014-11-01',end='2014-11-30',auto_adjust=False,period='1d',back_adjust=False)
+    print('hello')
--- analytics/prices_eda.py
+++ analytics/prices_eda.py
@@ -5,8 +5,8 @@ import numpy as np
 from data.NSEDataAccess import NSEMasterDataAccess
 from utils.plotters import line_plot, combine_plot
 from datetime import datetime
-from typing import Tuple
-from utils.config import PRICES_PKL_PATH,TICKER_METADATA_PATH
+from typing import Tuple,List
+from utils.config import YFINANCE_PRICES_PATH,TICKER_METADATA_PATH
 import os
 
 
@@ -18,7 +18,7 @@ def plot_prices(symbol: str, output_path: str, period: Tuple[datetime, datetime]
     :param period: start and end date in a tuple
     :return: None
     """
-    nse_data_access = NSEMasterDataAccess(output_path=PRICES_PKL_PATH)
+    nse_data_access = NSEMasterDataAccess(output_path=YFINANCE_PRICES_PATH)
     attrs_df = pd.read_csv(TICKER_METADATA_PATH)
     df = nse_data_access.get_prices(symbol=symbol, start_date=period[0], end_date=period[-1])
     df['year'] = df.index.year
@@ -32,7 +32,7 @@ def plot_prices(symbol: str, output_path: str, period: Tuple[datetime, datetime]
         std_dev = yearly_prices['returns'].std()
         mean = yearly_prices['returns'].mean()
         yearly_prices['return_zcore'] = abs((yearly_prices['returns']-mean)/std_dev)
-        fig_1 = line_plot(df=yearly_prices, column_list=['Open', 'Close', 'High', 'Low', 'VWAP', 'LastTradedPrice'],
+        fig_1 = line_plot(df=yearly_prices, column_list=['Open', 'Close', 'High', 'Low'],
                           data_type='Prices',title= f'{company_name}_{yr}',figsize=(10,10))
         fig_2 = line_plot(df=yearly_prices, column_list=['Close','AdjClose'],
                           data_type='Prices', title=f'{company_name}_{yr}', figsize=(10, 10))
@@ -52,50 +52,22 @@ def plot_prices(symbol: str, output_path: str, period: Tuple[datetime, datetime]
     combine_plot(fig_list=fig_list,output_path=output_path,file_name=f'{company_name}_eda_plots')
     return None
 
-
-def filter_corporate_actions_data(symbol_list:List[str], action_type:str)->pd.DataFrame:
-    """
-    :param symbol: list of stock symbols
-    :param action_type: one of dividend, bonus, split
-    :return: returns the historical corporate action of th stock symbol
-    """
-    if action_type not in ['dividend','bonus','split']:
-        raise ValueError(f'invalid action type, it should be one of [dividend,bonus,split] you passed {action_type}')
-
-    path_dict = {'bonus':BONUS_PATH_ALL,
-                 'split':SPLIT_PATH_ALL,
-                 'dividend':DIVIDEND_PATH_ALL
-                 }
-    df_list = []
-    for paths in path_dict[action_type].split('|'):
-        df = pd.read_csv(paths)
-        df_list.append(df)
-    df = pd.concat(df_list)
-    actions_df_list = []
-
-    for symbol in symbol_list:
-        action_df = df.loc[df['SYMBOL']==symbol]
-        actions_df_list.append(action_df)
-    action_df = pd.concat(actions_df_list)
-    return action_df
-
 if __name__=='__main__':
     output_path = '../EDA'
-    file_list = sorted(os.listdir("../additional_data/prices"))
-    file_list = file_list[24:]
-    period = (datetime(2002,1,1),datetime(2024,8,31))
+    file_list = sorted(os.listdir(f'{YFINANCE_PRICES_PATH}/prices/'))
+    file_list = file_list[0:25]
+    period = (datetime(2002,1,1),datetime(2024,10,17))
     failed_symbol = []
     for file in file_list:
         symbol = file.split("_")[0]
         try:
             plot_prices(symbol=symbol, output_path=output_path, period=period)
-        except:
+        except Exception as e:
+            print(e)
             failed_symbol.append(symbol)
 
     print(f"Symbols for which the code failed are {failed_symbol}")
-    # symbol_list = ["TATASTEEL"]
-    # for symbol  in symbol_list:
-    #     plot_prices(symbol=symbol,output_path=output_path,period=period)
+
 
 
 
