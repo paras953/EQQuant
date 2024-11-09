7ad9d72fafe99d5aebc0bad04aad5a38ab23d2a4
paras953 <parasdoshi953@gmail.com>
2024-11-09 16:06:18 +0530

    added execution at open,close backtesting functions

--- analytics/risk_return.py
+++ analytics/risk_return.py
@@ -2,8 +2,10 @@ import pandas as pd
 import numpy as np
 import os
 from utils.config import Columns
+from typing import Dict
 
-def get_returns(prices:pd.DataFrame,price_column:str=Columns.ADJ_CLOSE.value)->pd.DataFrame:
+
+def get_returns(prices: pd.DataFrame, price_column: str = Columns.ADJ_CLOSE.value) -> pd.DataFrame:
     """
     :param prices: a df having prices
     :param price_column: which prices you want to use to calculate the returns?
@@ -13,15 +15,39 @@ def get_returns(prices:pd.DataFrame,price_column:str=Columns.ADJ_CLOSE.value)->p
     return prices[['returns']]
 
 
-def get_volatility(prices:pd.DataFrame,price_column:str=Columns.ADJ_CLOSE.value)->pd.DataFrame:
+def get_volatility(prices: pd.DataFrame, price_column: str = Columns.ADJ_CLOSE.value) -> pd.DataFrame:
     """
     :param prices: a df having prices
     :param price_column: which prices you want to use to calculate the std?
     :return: a df having the volatility
     """
     prices['returns'] = prices[price_column].pct_change()
-    prices['volatility'] = prices['returns'].ewm(span=33,min_periods=33).std(bias=True)
+    prices['volatility'] = prices['returns'].ewm(span=33, min_periods=33).std(bias=True)
 
     first_valid_index = prices['volatility'].first_valid_index()
     prices = prices.truncate(first_valid_index)
     return prices[['volatility']]
+
+
+def get_asset_returns(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    :param df: where columns are the symbols and values are prices (can be open or close or vwap anything)
+    :return: df having returns
+    """
+    df = df.pct_change()
+    return df
+
+
+def get_intraday_returns(open_prices: pd.DataFrame, close_prices: pd.DataFrame) -> Dict[str, pd.DataFrame]:
+    """
+    :param open_prices: open prices per symbol
+    :param close_prices: close prices per symbol
+    :return: dict having OpenToClose and PrevDay CloseToOpen returns
+    """
+    open_to_close_returns = (-1 + (close_prices / open_prices))
+    prev_close_to_open_returns = (-1 + (open_prices / close_prices.shift()))
+    close_to_close_returns = get_asset_returns(close_prices)
+    return {'open_to_close_returns': open_to_close_returns,
+            'prev_day_close_to_open_returns': prev_close_to_open_returns,
+            'close_to_close_returns' : close_to_close_returns
+            }
--- utils/pandas_utils.py
+++ utils/pandas_utils.py
@@ -32,7 +32,7 @@ def clean_prices(df: pd.DataFrame, column: str, window_size: int = 11, threshold
     df.loc[mask_close_lt_low, :] = np.nan
 
     mask_volume_lt_eq_zero = df[Columns.VOLUME.value] <= 0
-    df = df[~mask_volume_lt_eq_zero]
+    df.loc[mask_volume_lt_eq_zero,:] = np.nan
 
     stats_dict['OPEN_GT_HIGH'] = mask_open_gt_high.mean() * 100
     stats_dict['CLOSE_GT_HIGH'] = mask_close_gt_high.mean() * 100
@@ -43,6 +43,10 @@ def clean_prices(df: pd.DataFrame, column: str, window_size: int = 11, threshold
     print("Stats dict, all figures are mentioned in percentage terms")
     print(stats_dict)
     df[Columns.ADJ_CLOSE.value] = df[Columns.ADJ_CLOSE.value].ffill()
+    df[Columns.CLOSE.value] = df[Columns.CLOSE.value] .ffill()
+    df[Columns.OPEN.value] = df[Columns.OPEN.value].combine_first(df[Columns.CLOSE.value])
+    df[Columns.HIGH.value] = df[Columns.HIGH.value].combine_first(df[Columns.CLOSE.value])
+    df[Columns.LOW.value] = df[Columns.LOW.value].combine_first(df[Columns.CLOSE.value])
     df = df.drop(columns=['rolling_mean', 'std_dev', 'zscore'])
     return df
 
